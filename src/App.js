1cf107d04c8bb44b456cf5b4a2c02432ffb7047e
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-15 11:21:45 +0530

    removed commend.

--- src/App.js
+++ src/App.js
@@ -10,30 +10,19 @@ function App() {
 }
 
 function ListDemo() {
-  // let list = ["delhi"]
-  // C1: Stateful Variable
   let [list, setList] = useState(["delhi"]);
 
-  // C2: Acton Member Funcn
   let addItemAction = () => {
-    // why didn u use push method, push method update the same object
-    // setter method requires immutable object.
-    // We have to pass new list.
     let newList = [...list, "mumbai"];
-    console.log(newList);
-
-    // DOM :: TRIGGER DOM
     setList(newList);
   };
 
   return (
     <>
-      {/** C3: Event Binding */}
       <input type="button" value="Add New Item" onClick={addItemAction} />
 
-      {/** C4: List */}
       {list.map((item) => (
-        <h1>Hello {item}</h1>
+        <h1>{item}</h1>
       ))}
     </>
   );
