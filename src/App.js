3cb20ab51e9eaa2812e5952c8a4fa5ed2d4c6ccd
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-16 09:55:42 +0530

    24

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 import MyNavigationLinks from "./pages/MyNavigationLinks";
@@ -18,9 +24,35 @@ function App() {
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
 
-        <MyNavigationLinks />
+        <MyNavigationLinksUsingButton />
       </BrowserRouter>
     </>
+  );
+}
+
+function MyNavigationLinksUsingButton() {
+  const navigate = useNavigate();
+
+  let goToHomeAction = () => {
+    navigate("/home");
+  };
+  let goToTodoAction = () => {
+    navigate("/todo");
+  };
+  let goToRegistrationAction = () => {
+    navigate("/registration");
+  };
+
+  return (
+    <div>
+      <input type="button" value="Home" onClick={goToHomeAction} />
+      <input type="button" value="Todo" onClick={goToTodoAction} />
+      <input
+        type="button"
+        value="Registartion"
+        onClick={goToRegistrationAction}
+      />
+    </div>
   );
 }
 
