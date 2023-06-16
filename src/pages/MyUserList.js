02b4fcde2c19ff3a04c99d015fe8999ef3bb8baa
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-16 13:52:05 +0530

    user list.

--- src/pages/MyUserList.js
+++ src/pages/MyUserList.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function MyUserList() {
+  const [userList, setUserList] = useState([]);
+
+  const getAllUserAction = async () => {
+    let url = `http://localhost:4000/find-all-user`;
+    let res = await fetch(url);
+    let list = await res.json();
+
+    setUserList([...list]);
+  };
+
   return (
     <>
       <h1>User List</h1>
+      <input type="button" value="Get All User " onClick={getAllUserAction} />
+
+      {userList.map((item) => (
+        <>
+          <div>
+            {item.username} {item.password} {item.email} {item.mobile}
+          </div>
+        </>
+      ))}
     </>
   );
 }
