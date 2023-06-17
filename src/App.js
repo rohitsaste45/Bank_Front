4403273d73f1d6ed2e57f3b272c51c3b53ca96f3
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-17 19:39:12 +0530

    pages are not protectd.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 import MyNavigationLinks from "./pages/MyNavigationLinks";
@@ -18,17 +18,60 @@ function App() {
           {/** 1 Route means 1 Page */}
           <Route path="/" element={<h1>Hello</h1>} />
           <Route path="/login" element={<AppLogin />} />
-          <Route path="/home" element={<AppHome />} />
-          <Route path="/todo" element={<MyTodo />} />
-          <Route path="/todo-list" element={<MyTodoList />} />
+
+          {/** Private Needs Protection */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <AppHome />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/todo"
+            element={
+              <ProtectedRoute>
+                <MyTodo />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/todo-list"
+            element={
+              <ProtectedRoute>
+                <MyTodoList />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/user-list"
+            element={
+              <ProtectedRoute>
+                <MyUserList />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="/registration" element={<MyRegistration />} />
-          <Route path="/user-list" element={<MyUserList />} />
           <Route path="/validation-demo" element={<ValidationDemo />} />
+
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </>
   );
+}
+
+// lets protect the pages
+function ProtectedRoute({ children }) {
+  let loginStatus = localStorage.getItem("loginStatus");
+  if (!loginStatus) {
+    return <Navigate to={"/login"} replace={true} />;
+  }
+
+  return children;
 }
 
 export default App;
