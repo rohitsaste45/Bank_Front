e467e473cad6b56a886be26435ff0fbc6e4efa9f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-17 11:09:56 +0530

    navigation menu update.

--- src/pages/MyNavigationLinks.js
+++ src/pages/MyNavigationLinks.js
@@ -1,14 +1,37 @@
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function MyNavigationLinks() {
   return (
     <>
-      <Link to={"/home"}>Home |</Link>
-      <Link to={"/todo"}>Todo |</Link>
-      <Link to={"/todo-list"}>Todo List |</Link>
-      <Link to={"/registration"}>Registration |</Link>
-      <Link to={"/user-list"}>User List |</Link>
-      <Link to={"/validation-demo"}>ValidationDemo</Link>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">My Project</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={"/home"}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/todo"}>
+                Todo
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/todo-list"}>
+                Todo List
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/registration"}>
+                Registration
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/user-list"}>
+                User List
+              </Nav.Link>
+              <Nav.Link as={Link} to={"/validation-demo"}>
+                ValidationDemo
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
